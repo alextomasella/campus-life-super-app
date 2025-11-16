87b149f611626bf9e779a1b99f4f249d8d2cf300
alextomasella <alextomasella249@gmail.com>
2025-11-16 17:10:53 -0600

    started an API for an events. API got from the free API link given.

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"<!--Incomplete API for event retrieval-->"
+
+let endpoint = "https://www.eventbriteapi.com/v3/users/me/?token=5A56SHW7FQLTMQX455BV";
+console.log(endpoint);
+
+async function getEvents() {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const images = data.data.map(gif => gif.images.original.url);
+    console.log(images);
+
+    const output = document.getElementById("gif-container");
+    output.innerHTML = "";
+
+    for (let url of images) {
+        output.innerHTML += `<img src="${url}" class="col-3 mb-3">`;
+    }
+}
+
+const button = document.querySelector("#see-events-btn");
+const eventsContainer = document.querySelector("#events-container");
+button.addEventListener("click", function() {
+    getEvents();
+});
